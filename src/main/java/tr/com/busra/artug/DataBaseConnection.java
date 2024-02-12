0d4335da2a9521d2626081c80ca647394374b590
Busra Artug <busra.artug@bites.com.tr>
2024-02-12 10:51:42 +0300

    ilkcalisma

--- src/main/java/tr/com/busra/artug/DataBaseConnection.java
+++ src/main/java/tr/com/busra/artug/DataBaseConnection.java
@@ -1,2 +1,37 @@
-package tr.com.busra.artug;public class DataBaseConnection {
+package tr.com.busra.artug;
+
+//Singleton Design
+public class DataBaseConnection {
+
+    private static DataBaseConnection instance;
+    private String connectionString;
+
+    public static void setInstance(DataBaseConnection instance) {
+        DataBaseConnection.instance = instance;
+    }
+
+    public String getConnectionString() {
+        return connectionString;
+    }
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    public DataBaseConnection(){
+        connectionString = "jdbc:mysql://localhost:3306/mydatabase";
+    }
+    public static DataBaseConnection getInstance(){
+        if(instance == null){
+            instance = new DataBaseConnection();
+        }
+        return instance;
+    }
+    public void connect(){
+        System.out.println("Veritabanına bağlandı" + connectionString);
+    }
+    public void disconnect(){
+        System.out.println("Veri tabanından çıkıldı");
+
+    }
 }
